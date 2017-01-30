6ab36d81a1825755994cbb40d320f321b199ffd6
Sandeep <sandeep.kumar0486@gmail.com>
2017-01-30 10:46:44 +0530

    auto-update change

--- app.js
+++ app.js
@@ -7,6 +7,108 @@ const {app} = electron
 // Module to create native browser window.
 const {BrowserWindow} = electron;
 
+const autoUpdater = require('electron').autoUpdater;
+const appVersion = require('./package.json').version;
+const os = require('os').platform();
+
+var updateFeed = 'http://localhost:3000/releases/win32/0.0.2/Autographa';
+
+autoUpdater.setFeedURL(updateFeed);
+autoUpdater.checkForUpdates();
+// autoUpdater.quitAndInstall();
+
+// autoUpdater.on('error', (e) => {
+//     console.error(e.message)
+//   });
+
+//   autoUpdater.on('checking-for-update', () => {
+//     console.info('Checking for update...')
+//   });
+
+//   autoUpdater.on('update-available', () => {
+//     console.info('Found available update!')
+//   });
+
+//   autoUpdater.on('update-not-available', () => {
+//     console.info('There are no updates available.')
+//   });
+
+  autoUpdater.on('update-downloaded', () => {
+    console.info('update downloaded.');
+    autoUpdater.quitAndInstall();
+  });
+
+
+
+
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Setup.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
@@ -80,6 +182,17 @@ function preProcess() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', preProcess);
+
+
+
+// if (process.env.NODE_ENV !== 'development') {
+//   updateFeed = os === 'darwin' ?
+//     'https://mysite.com/updates/latest' :
+//     'http://localhost/releases/win32';
+// }
+
+
+
 
 
 // Quit when all windows are closed.
